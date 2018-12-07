53fe7382ada96e87ecfdc124246b7e75d937b397
yoloroy <slart_ru@mail.ru>
2018-12-07 21:55:22 +0300

    now all functions are similar
    (next step - one function for update, draw and other functions)

--- develop/menu.lua
+++ develop/menu.lua
@@ -4,31 +4,45 @@ width = love.graphics.getWidth()
 height = love.graphics.getHeight()
 
 menu.play = button:new(width/2, height/30*4, "/png/bb_play.png")
+menu.play.func = function ()
+                     scene = 'race'
+                 end
 menu.tuning = button:new(width/2, height/30*10, "/png/bb_tuning.png")
+menu.tuning.func = function ()
+                     scene = 'tuning'
+                 end
 menu.settings = button:new(width/2, height/30*16, "/png/bb_settings.png")
+menu.settings.func = function ()
+                     scene = 'settings'
+                 end
 menu.exit = button:new(width/2, height/30*26, "/png/bb_exit.png")
+menu.exit.func = function ()
+                     love.event.quit()
+                 end
 
 menu.buttons = {
   menu.play, menu.tuning, menu.settings, menu.exit}
 
 function menu.draw()
-    for _, item in pairs(menu.buttons) do
-        item:draw()
+    for _, button in pairs(menu.buttons) do
+        button:draw()
     end
 end
 
 function menu.update(dt)
-    for _, item in pairs(menu.buttons) do
-        item:update(dt)
+    for _, button in pairs(menu.buttons) do
+        button:update(dt)
     end
 end
 
 function menu.mpress(x, y)
-    if menu.play:mpress(x, y) then
-        scene = "race"
-        --car1 = car:new(width/4-188, height/2-100, 1600, 1000, {600, 375}, {255, 255, 255, 255}, "loshadka1.png", sets['carsprites1'], sets['carcolors1'], 1, dial, {'q','e','space'})
-        --car2 = car:new(width/4-188, height/2-35, 1600, 1000, {600, 375}, {255, 255, 255, 255}, "loshadka2.png", sets['carsprites2'], sets['carcolors2'], 1, dial, {'[',']','ralt'})
-      
+    for _, button in pairs(menu.buttons) do
+        if button:mpress(x, y) then
+            button.func()
+        end
+    end
+    
+    if scene == "race" then
         background11 = background:new(0, 0, {width, height}, {255, 255, 255, 255}, 
                                            {11520, 1048}, "/png/track.png")
         background12 = background:new(0, 0, {width, height}, {255, 155, 155, 255}, 
@@ -40,11 +54,5 @@ function menu.mpress(x, y)
     
         session1 = session:new(0,       0, car1, car2, background11, background12, "/png/satrt.png", 1000*24, 0)
         session2 = session:new(width/2, 0, car2, car1, background21, background22, "/png/satrt.png", 1000*24, 1)
-    elseif menu.tuning:mpress(x, y) then
-        scene = "tuning"
-    elseif menu.settings:mpress(x, y) then
-        scene = "settings"
-    elseif menu.exit:mpress(x, y) then
-        love.event.quit()
     end
 end
